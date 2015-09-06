20954e6dc2ae68d2bde96e3e120ba1d0bdb0f5bc
Håkon Treider <haakonvt@users.noreply.github.com>
2015-09-07 01:41:49 +0200

    Update elastic_pendulum.py

--- Compulsory Assignment 01/elastic_pendulum.py
+++ Compulsory Assignment 01/elastic_pendulum.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def simulate(
     beta=0.9,                 # dimensionless parameter
-    Theta=30,                  # initial angle in degrees
+    Theta=30,                 # initial angle in degrees
     epsilon=0,                # initial stretch of wire
     num_periods=6,            # simulate for num_periods
     time_steps_per_period=60, # time step resolution
